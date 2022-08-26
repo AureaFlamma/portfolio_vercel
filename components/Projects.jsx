fe1567fca66c771a2faac2ebe03614fa1264e68b
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-08-26 13:23:15 +0100

    tings be working yo

---
+++ components/Projects.jsx
@@ -0,0 +1,9 @@
+import { Flex, Spacer } from "@chakra-ui/react";
+import React from "react";
+
+function Projects() {
+  return <Flex bg="green">fsdfdsngsn</Flex>;
+}
+
+export default Projects;
+// m="clamp(50px, 5vh, 5vh)" my="clamp(25px, 2.5vh, 2.5vh)"
---
+++ components/TextCard.jsx
@@ -0,0 +1,23 @@
+import { Flex, Spacer, Text } from "@chakra-ui/react";
+import React from "react";
+
+function TextCard({ title, subtitle, paragraph }) {
+  return (
+    <Flex h="90vh" direction="column" bg="blackAlpha.100">
+      <Text fontSize="120" fontWeight="700" lineHeight={"1"}>
+        {title}
+      </Text>
+      <Text fontSize="120" fontWeight="600" lineHeight={"1"}>
+        {subtitle}
+      </Text>
+      <Text fontSize="30" mt="4em" w="80%" textAlign={"justify"}>
+        {paragraph}
+      </Text>
+    </Flex>
+  );
+}
+
+export default TextCard;
+
+// m="clamp(50px, 5vh, 5vh)"
+// mb="clamp(25px, 2.5vh, 2.5vh)"
