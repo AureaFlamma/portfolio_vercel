a3fd0badef04b495df762cd9c316fc04ace8ae1b
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-09-01 16:46:35 +0100

    some minor styling changes

--- pages/index.js
+++ pages/index.js
@@ -8,31 +8,27 @@ import PageNav from "../components/PageNav";
 import styles from "./background.module.css";
 export default function Index() {
   return (
-    <div>
-      <Flex
-        className={styles.gradient}
-        direction="column"
-        py="110px"
-        px="110px"
-        // bgGradient="linear(130deg, #0B315E 0%, #FF0E56 74.31%, #FFE70E 103.68%)"
-        // backgroundAttachment={"fixed"}
-        color="white"
-        gap="110px"
-      >
-        <PageNav />
-        <TextCard
-          title={Lorem.name}
-          subtitle={Lorem.role}
-          paragraph={Lorem.personal_profile}
-          id="home"
-        />
-        {/* <Spacer my="40px" /> */}
-        <Projects id="projects" />
-        {/* <Spacer my="40px" /> */}
-        <TextCard subtitle={`About`} paragraph={Lorem.about} id="about" />
-        <SocialsNav />
-      </Flex>
-    </div>
+    <Flex
+      className={styles.gradient}
+      direction="column"
+      py="110px"
+      px="110px"
+      color="white"
+      gap="110px"
+    >
+      <PageNav />
+      <TextCard
+        title={Lorem.name}
+        subtitle={Lorem.role}
+        paragraph={Lorem.personal_profile}
+        id="home"
+      />
+
+      <Projects id="projects" />
+
+      <TextCard subtitle={`About`} paragraph={Lorem.about} id="about" />
+      <SocialsNav />
+    </Flex>
   );
 }
 
