c33e51b9305a7b1f55c18eb138fc5bc3c21873fb
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-09-06 17:12:57 +0100

    tweaked About

--- libs/Lorem.js
+++ libs/Lorem.js
@@ -2,9 +2,9 @@ const Lorem = {
   name: "Philip Kaminski",
   role: `Full Stack Developer`,
   personal_profile:
-    "Hey, my name's Philip. I'm a full stack software developer and recent Psychology BSc graduate who believes there are more ways than one to help those around us. I combine a passion for creating beautiful, functional, innovative and accessible designs with a love for data, problem-solving and scientific approach. Currently looking for work",
+    "Full stack software developer and recent Psychology BSc graduate who believes there are more ways than one to help those around us. I combine a passion for creating beautiful, functional, innovative and accessible designs with a love for data, problem-solving and scientific approach. Currently looking for work.",
   about:
-    "Hey, my name is Philip. I’m a junior full stack developer. I love creating stunning designs and neat functionality on the frontend with data and the backend being my very close second favourite! I work best in Agile teams and am familiar with GitHub Flow, version control and code reviews. My stack includes React.js, Next.js, Express.js, SQL, CSS, Tailwind, Chakra UI and much more! I’m always on the lookout for new challenging projects to contribute to, so make sure to contact me if you’d like to collaborate!",
+    "I love creating stunning designs and neat functionality on the frontend with data and the backend being my very close second favourite! I just finished the amazing School of Code bootcamp where I spent 16 weeks pair programming in agile teams and delivering cool software (which you can see above). My stack includes React.js, Next.js, Express.js, SQL, CSS, Tailwind, Chakra UI and much more! I work best in Agile teams and am familiar with GitHub Flow, version control and code reviews. I’m always on the lookout for new challenging projects to contribute to, so make sure to contact me if you’d like to collaborate!",
 };
 
 export default Lorem;
